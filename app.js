5a79563db8272571c77960e66ad43d0eb80a2f91
ehtisham-altaf <ehtisham.altaf@aiondigital.com>
2024-03-28 21:00:57 +0500

    attempt 2

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const dotenv = require("dotenv");
-dotenv.config();
+// server.js
+
 const express = require("express");
-const connectToMongo = require("./database/db");
 const path = require("path");
+const connectToMongo = require("./database/db");
 
 const app = express();
 
 // Connect to MongoDB
 connectToMongo();
 
-// Define port
-const appPort = process.env.PORT || 2000;
-
 // Middleware for JSON parsing
 app.use(express.json({ limit: "50mb" }));
 
 // Serve static files from the client directory
-app.use(express.static(path.join(__dirname, 'client', 'build v2')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // Routes
 app.use("/api/auth", require("./router/auth"));
@@ -24,22 +21,11 @@ app.use("/api/products", require("./router/product_route"));
 
 // Catch all route to serve the index.html for any other route
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build v2', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 // Start the server
-const server = app.listen(appPort, () => {
-  console.log(`Backend server is up on port ${appPort}`);
-});
-
-// Error handling
-process.on("uncaughtException", (err) => {
-  console.error(`Uncaught Exception: ${err.stack}`);
-  server.close(() => process.exit(1));
-});
-
-// Handle other unexpected errors
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-  // Recommended: log the error and close the server gracefully
+const PORT = process.env.PORT || 2000;
+app.listen(PORT, () => {
+  console.log(`Backend server is up on port ${PORT}`);
 });
